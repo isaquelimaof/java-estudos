875176d131a71a80112335240df498380bb9502b
isaquelimaof <isaquelimaof@gmail.com>
2021-09-21 08:11:08 -0300

    Modificando arquivos

--- src/progdoc/estudos/ProgUsuarioPagamento.java
+++ src/progdoc/estudos/ProgUsuarioPagamento.java
@@ -4,15 +4,15 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 
 public class ProgUsuarioPagamento {
 
-    private static Scanner dataUsuario;
+    public static Scanner dataUsuario;
 
-    public static void main(String[] args, Scanner scanner) throws ParseException {
+    public static void main(String[] args) throws ParseException {
 
-        dataUsuario = scanner;
+        
+        dataUsuario = new Scanner(System.in);
         System.out.println("Por favor escreva a data do vencimento: ");
         String date = dataUsuario.nextLine();
 
@@ -21,12 +21,18 @@ public class ProgUsuarioPagamento {
 
         Date dataHoje = new Date();
 
-        long calculDatas = dataVenc.getTime() - dataHoje.getTime();
+        long calculDatas = dataVenc.getDate() - dataHoje.getDate();
 
-        TimeUnit time = TimeUnit.DAYS;
-        long diffrence = time.convert(calculDatas, TimeUnit.MILLISECONDS);
-        long resultado = diffrence + 1;
-        System.out.println("Você tem: " + resultado + " dias para efetuar o pagamento da sua fatura!!!");
+        if (calculDatas<0) {
+            System.out.println("!!!!!!Atenção!!!!!!\n"+"!!!!!!!Sua conta esta vencida!!!!!!");
+        
+        }if(calculDatas>0){
+            System.out.println("Vc tem : "+calculDatas+" dias para pagar sua conta!!!!!");
 
+        }if(calculDatas == 0) {
+            System.out.println("!!!!Atenção!!!!!\n   "+"Hoje é o ultimo dia para pagamento da sua conta!!!!");
+        }
+        
+
+        }
     }
-}
