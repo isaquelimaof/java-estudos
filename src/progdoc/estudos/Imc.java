6adda6e966af75034dd349b645254bf43c901efe
isaquelimaof <isaquelimaof@gmail.com>
2021-09-23 00:36:17 -0300

    Alterando arquivos 03

---
+++ src/progdoc/estudos/Imc.java
@@ -0,0 +1,53 @@
+package progdoc.estudos;
+
+import java.util.Scanner;
+
+public class Imc {
+     
+    public static Scanner usuario;
+
+    public static void main(String[] args) {
+
+        usuario = new Scanner(System.in);
+
+        System.out.println("Qual seu peso: ");
+        float peso = usuario.nextFloat();
+
+        System.out.println("Qual sua altura: ");
+        float h = usuario.nextFloat();
+
+        float imc = peso / (h * h);
+        System.out.println("Seu IMC é: " + imc);
+
+        if (imc < 17.5) {
+            System.out.println("Quando seu IMC está abaixo de 17 \n" +
+                    "-->!!! O seu peso está muito abaixo do ideal !!!");
+        }
+        if (imc >= 17.5 & imc <= 18.49 ) {
+            System.out.println("Quando seu IMC está entre 17,5 e 18,49 \n" +
+                    "-->  O seu peso está abaixo do ideal.");
+        }
+        if (imc >= 18.5 & imc <= 24.99) {
+            System.out.println("Quando seu IMC está entre 18,5 e 24,99 \n" +
+                    "-->  O seu peso está ideal.");
+        }
+        if (imc >= 25 & imc <= 29.99) {
+            System.out.println("Quando seu IMC está entre 25 e 29,99 \n" +
+                    "-->  O seu peso está acima do ideal.");
+        }
+        if (imc >= 30 & imc <= 34.99) {
+            System.out.println("Quando seu IMC está entre 30 e 34,99 \n" +
+                    "-->  O seu peso está em obesidade grau I (!!!Atenção!!!)");
+        }
+        if (imc >= 35 & imc <= 39.99) {
+            System.out.println("Quando seu IMC está entre 35 e 39,99 \n" +
+                    "-->  O seu peso está em obesidade grau II (severa) (!!!Atenção!!!)");
+        }
+        if (imc >= 40) {
+            System.out.println("Quando seu IMC está acima de 40 \n" +
+                    "-->  O seu peso está em obesidade grau III (mórbida) (!!!Atenção!!!)");
+        }
+
+    }
+
+}
