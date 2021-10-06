84d27e308ea6316f3b7fc4ac75c6c04dbb9abac0
isaquelimaof <isaquelimaof@gmail.com>
2021-10-05 21:42:38 -0300

    Alterando nome de pastas

--- src/javaInnovationOne/estudos/innovationOne/basicoAoAvancado/TesteIteracoes.java
+++ src/javaInnovationOne/estudos/innovationOne/basicoAoAvancado/TesteIteracoes.java
@@ -6,19 +6,20 @@ import  java.util.Scanner;
 
 public class TesteIteracoes {
     public static void main(String[] args) {
+        String[] nomes = {"Joao","Joao", "Paulo","Oliveira","Santos","Instrutor","Java"};
+        Integer[] numeros = {1,2,3,4,5};
+//        imprirmirNomesFiltrados(nomes);
+//        imprirmirTodosNomes(nomes);
+//        imprirmirODobroDeCadaItemDaLista(numeros);
 
         List<String> profissoes = new ArrayList<>();
-
-        Scanner usuario = new Scanner(System.in);
-        String usuario2 = dataUsuario.lines();
-
         profissoes.add("Desenvolvedor");
         profissoes.add("Testador");
         profissoes.add("Gerente de projeto");
         profissoes.add("Gerente de qualidade");
 
         profissoes.stream()
-                .filter(profissao -> profissao.startsWith(String.valueOf(usuario2.lines())))
+                .filter(profissao -> profissao.startsWith("Gerente"))
                 .forEach(System.out::println);
 
 
